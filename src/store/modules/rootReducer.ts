ee1da5c28d2d44087c20f3cf3bcb9ec3e3540dd2
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-24 19:07:01 -0300

    [refactor] alteração de nome

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { combineReducers } from "@reduxjs/toolkit";
-import filmsReducer from "./filmsSlice";
+import filmsSlice from "./filmsSlice";
 import peopleSlice from "./peopleSlice";
 import planetsSlice from "./planetsSlice";
 
 export default combineReducers({
-  films: filmsReducer,
+  films: filmsSlice,
   people: peopleSlice,
   plantets: planetsSlice,
 });
